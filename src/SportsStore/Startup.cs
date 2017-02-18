53b5160185e5bdffe448fe7f252e915de1b1ae97
David Walker <david.lennox@live.com>
2017-02-18 23:05:07 +1100

    squash this into next.

--- src/SportsStore/Startup.cs
+++ src/SportsStore/Startup.cs
@@ -9,7 +9,15 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace SportsStore {
-
+    /// <summary>
+    /// The Startup class is very important. In here you instantiate all the services and middleware. 
+    /// 
+    /// Middleware are software components that are assembled into an application pipeline to handle requests and responses.Each component chooses whether to pass the request on to the next component in the pipeline, and can perform certain actions before and after the next component is invoked in the pipeline. Request delegates are used to build the request pipeline.The request delegates handle each HTTP request.
+    /// 
+    /// Request delegates are configured using Run, Map, and Use extension methods on the IApplicationBuilder instance that is passed into the Configure method in the Startup class. An individual request delegate can be specified in-line as an anonymous method (called in-line middleware), or it can be defined in a reusable class. These reusable classes and in-line anonymous methods are middleware, or middleware components.Each middleware component in the request pipeline is responsible for invoking the next component in the pipeline, or short-circuiting the chain if appropriate.+
+    /// 
+    /// Migrating HTTP Modules to Middleware explains the difference between request pipelines in ASP.NET Core and the previous versions and provides more middleware samples.
+    /// </summary>
     public class Startup {
         IConfigurationRoot Configuration;
 
